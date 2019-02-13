028678c1fe48c8455ecb63668c8e281dcab484ee
jmmongeau <jmmongeau@psu.edu>
2019-02-13 12:43:30 -0500

    Update BladeTestRectangleV3.m

--- BladeTestRectangleV3.m
+++ BladeTestRectangleV3.m
@@ -10,20 +10,20 @@ function [L, D, dL, dD]=BladeTestRectangleV3(C_L,C_D,a,b,ne,T)
 %a and b are the wing width and length
 %% constants
 %r=b/ne*(0:ne-1)+b/(2*ne);      %distance from base of wing to middle of element
-dr=b/ne;     %width of each wing element
-Cr=a;       %chord length of each element. Since I am using a rectangle then it is the same for all elements
-rho=1.225; %density of air
-phi=120; % deg turned into rad in dL calcualtions
+dr = b/ne;     %width of each wing element
+Cr = a;       %chord length of each element. Since I am using a rectangle then it is the same for all elements
+rho = 1.225; %density of air at Standard Atmosphere (kg/m^3)
+phi = 120; % deg turned into rad in dL calcualtions
 
-n=1/T;
+n = 1/T;
 syms t % testing to integrate t rather than sum
 
 syms r
 %% calculation of the lift and drag for each element of the wing
 %(equation checked as of v2)
 
-dL=0.5*C_L*rho*pi^2*n^2*(phi*pi/180)^2*Cr*r^2*(cos(2*pi*n*t))^2;
-dD=0.5*C_D* rho*pi^2*n^2*(pi*phi/180)^2*Cr*r^2*(cos(2*pi*n*t))^2;
+dL = 0.5*C_L*rho*pi^2*n^2*(phi*pi/180)^2*Cr*r^2*(cos(2*pi*n*t))^2;
+dD = 0.5*C_D* rho*pi^2*n^2*(pi*phi/180)^2*Cr*r^2*(cos(2*pi*n*t))^2;
 
 L=int(dL,r);
 D=int(dD,r);
